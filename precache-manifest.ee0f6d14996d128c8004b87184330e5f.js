dabca07418d2e5cad890bb5005583c3c3c6cd1e6
Xeeno <sahitsrivyshnav1999@gmail.com>
2019-09-22 20:19:03 +0530

    Updates

---
+++ precache-manifest.ee0f6d14996d128c8004b87184330e5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4233220c19dd01f6183090799c94860a",
+    "url": "/monster-search/index.html"
+  },
+  {
+    "revision": "ef89f969baf97659b7a2",
+    "url": "/monster-search/static/css/main.160d50e1.chunk.css"
+  },
+  {
+    "revision": "0792b5e2043d95efda44",
+    "url": "/monster-search/static/js/2.1159b1ef.chunk.js"
+  },
+  {
+    "revision": "ef89f969baf97659b7a2",
+    "url": "/monster-search/static/js/main.85919b09.chunk.js"
+  },
+  {
+    "revision": "33b58072f57acefd9955",
+    "url": "/monster-search/static/js/runtime-main.fbae94b6.js"
+  }
+]);
